dbf2651b8cc937e135f84ecf339c042e4cf070de
kysely-matteo <xkysely@stuba.sk>
2025-12-03 15:04:04 +0100

    Delete front directory

--- front/src/main/java/sk/front/front/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package sk.front.front.config;
-
-public class AppConfig {
-    public static final String API_BASE_URL = "http://localhost:8080/api";
-
-    public static String getApiUrl(String endpoint) {
-        return API_BASE_URL + endpoint;
-    }
-}
